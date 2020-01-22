ee1e8318508070da870e2d380296f2a250081239
heliolidoni <heliolidoni@hotmail.com>
2020-01-22 15:37:21 -0300

    Teste DC

--- Web/Controllers/WeatherForecastController.cs
+++ Web/Controllers/WeatherForecastController.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Logging;
 
 namespace Web.Controllers
 {
+	///Vai pro Git Helio Lidoni
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
